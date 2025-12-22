5e1f0f580ee881105a6a73f177c89ab8975bcfd3
stormrising <rg.stormrising@gmail.com>
2025-12-22 19:23:08 +0530

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -2,7 +2,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    {
+   {
       text: 'What we do',
       href: getPermalink('/#whatwedo'),
     },
@@ -18,20 +18,22 @@ export const headerData = {
       text: 'Contact',
       href: getPermalink('/#contact'),
     },
-    
-    
-   
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
+  actions: [{ text: 'Contact', href: 'https://github.com/arthelokyo/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
+  links: [
+    
+  ],
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  
+  socialLinks: [
+   
+  ],
   footNote: `
-    © 2025 ANRF· All rights reserved.
+     © 2025 ANRF· All rights reserved.
   `,
 };
